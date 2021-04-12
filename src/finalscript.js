044d509570befe046e6a6287a2f0a32685833b78
veggiekenz <veggiekenz@gmail.com>
2021-04-12 14:53:17 -0700

    try to fix description error

--- src/finalscript.js
+++ src/finalscript.js
@@ -47,10 +47,9 @@ function displayTemperature(response) {
     let humidityElement = document.querySelector("#humidity");
     imperialElement.innerHTML = Math.round
     (response.data.main.temp);
-    cityElement.innerHTML =
-    (response.data.main.name);
-     descriptionElement.innerHTML =
-    (response.data.weather[0].description);
+    cityElement.innerHTML = response.data.main.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
     }
 
 let units = "imperial";
