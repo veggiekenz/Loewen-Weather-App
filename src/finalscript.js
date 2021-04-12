621b8ffd62d4124050089e19b838316f82b8ab97
veggiekenz <veggiekenz@gmail.com>
2021-04-12 14:34:14 -0700

    scrapped previous api code. starting simple

--- src/finalscript.js
+++ src/finalscript.js
@@ -40,38 +40,14 @@ let currentTime = `${hours}:${minutes}`;
 currentDate.innerHTML = `${dayOfWeek}, ${currentMonth} ${date}`;
 time.innerHTML = `${currentTime}`;
 
-function displayWeatherCondition(response) {
-    document.querySelector("#city").innerHTML = response.data.name;
-    document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.main.temp);
+function displayTemperature(response) {
+    console.log(response.data.main.temp);
     }
 
-
-function search(event) {
-    event.preventDefault();
-    let cityInput = document.querySelector("#city").value;
     let units = "imperial";
     let apiKey = "024eca5cf1cd2fe74cef469e2a03433b";
     let apiEndPoint = "https://api.openweathermap.org/data/2.5/weather";
-    let apiUrl = `${apiEndPoint}?q=${cityInput}&appid=${apiKey}&units=${units}`;
-    axios.get(apiUrl).then(displayWeatherCondition);
-    }
-
-let form = document.querySelector("#city-input");
-form.addEventListener("submit", search);
-
-function showHere(event) {
-    event.preventDefault();
-    navigator.geolocation.getCurrentPosition(showPosition);
-
-    }
-
-function showPosition(position) {
-    let lon = position.coords.longitude;
-    let lat = position.coords.latitude;
-    let units = "imperial";
-    let apiKey = "024eca5cf1cd2fe74cef469e2a03433b";
-    let apiEndPoint = "https://api.openweathermap.org/data/2.5/weather";
-    let apiUrl = `${apiEndPoint}?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
-    axios.get(apiUrl).then(displayWeatherCondition);
-    }
+    let apiUrl = `${apiEndPoint}?q=Phoenix&appid=${apiKey}&units=${units}`;
+    
+    axios.get(apiUrl).then(displayTemperature);
+    
