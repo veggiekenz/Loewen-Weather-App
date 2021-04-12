9aa24caadec6df7f162d8b7e9221bc12928e780c
veggiekenz <veggiekenz@gmail.com>
2021-04-12 13:21:57 -0700

    Working on JS date

--- src/finalscript.js
+++ src/finalscript.js
@@ -3,41 +3,41 @@ let day = now.getDay();
 let month = now.getMonth();
 let date = now.getDate();
 let hours = now.getHours();
-if (hours < 10) {
-hours = `0${hours}`;
-}
+    if (hours < 10) {
+    hours = `0${hours}`;
+    }
 let minutes = now.getMinutes();
-if (minutes < 10) {
-minutes = `0${minutes}`;
-}
+    if (minutes < 10) {
+    minutes = `0${minutes}`;
+    }
 let days = [
-"Sunday",
-"Monday",
-"Tuesday",
-"Wednesday",
-"Thursday",
-"Friday",
-"Saturday"
-];
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+    ];
 let dayOfWeek = days[now.getDay()];
 let months = [
-"January",
-"February",
-"March",
-"April",
-"May",
-"June",
-"July",
-"August",
-"September",
-"October",
-"November",
-"December"
-];
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+    ];
 let currentMonth = months[now.getMonth()];
-let todayDate = `${dayOfWeek}, ${currentMonth} ${date}`;
 let currentTime = `${hours}:${minutes}`;
-currentDate.innerHTML = `${todayDate}`;
+
+currentDate.innerHTML = `${dayOfWeek}, ${currentMonth} ${date}`;
 time.innerHTML = `${currentTime}`;
 
 function displayWeatherCondition(response) {
