38088a05dcd95cb3c3bfdef9836e5685f8935cc9
veggiekenz <veggiekenz@gmail.com>
2021-04-18 11:58:14 -0700

    fixed error

--- src/finalscript.js
+++ src/finalscript.js
@@ -46,11 +46,11 @@ function displayForecast() {
     let days = ["Sun","Mon","Tues","Wed"];
 
     let forecastHTML = `<div class="row">`;
-    days.forEach(function (day) {
+    days.forEach(function (days) {
         forecastHTML = forecastHTML + `
                     <div class="col-2">
                         <div class="weather-forecast-date">
-                        ${day}</div>
+                        ${days}</div>
                          <img src="src/download.png" alt="dummy image" width="42"/>
                         <div class="weather-forecast-temperatures">
                             <span class="weather-forecast-temperature-max"> 18° </span>
