08c84e4dc20bebe2c61094019a030df4aaa598e6
veggiekenz <veggiekenz@gmail.com>
2021-04-18 12:43:26 -0700

    error checking apiurl

--- src/finalscript.js
+++ src/finalscript.js
@@ -89,7 +89,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
     let apiKey = "024eca5cf1cd2fe74cef469e2a03433b";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric}`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayForecast);
 }
 
